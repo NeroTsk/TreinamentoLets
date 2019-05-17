df1194e4e992934e8db2a72bdacd272ed18fe075
NeroTsk <48331544+NeroTsk@users.noreply.github.com>
2019-05-17 14:08:40 -0300

    Fix
    Realizado mudanças de acordo as observações tomadas

--- PrimeiroDesafio/formularioEnviar.php
+++ PrimeiroDesafio/formularioEnviar.php
@@ -1,24 +1,24 @@
-<?php include('header.php') ?>
+<?php include('header.php');
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telephone'];
+    $assunto = $_POST['topic'];
+    $menssagem = $_POST['message'];
+?>
 
 <div class="row justify-content-center">
     <div class="card text-white bg-dark" style="width: 26rem;">
         <div class="card-body">
-            <?php
-
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $telefone = $_POST['telephone'];
-            $assunto = $_POST['topic'];
-            $msn = $_POST['message'];
-
-            $texto = "Nome: \n {$name} \n <hr>". 
-            "Email: \n {$email} \n <hr>". 
-            "telefone: \n {$telefone} \n <hr>".
-            "assunto: \n {$assunto} \n <hr>".
-            "Mensagem: \n {$msn}" ;
-
-            echo nl2br($texto);
-            ?>
+            Nome: 
+            <p> <?= $name ?> </p>
+            Email:
+            <p> <?= $email ?> </p> 
+            Telefone:
+            <p> <?= $telefone ?> </p>
+            Assunto:
+            <p> <?= $assunto ?> </p>
+            Mensagem:
+            <p> <?= $menssagem ?> </p>
         </div>
     </div>
 </div>
